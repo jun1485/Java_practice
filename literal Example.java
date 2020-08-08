ab25a4d2d6e0ec4d28f0559510d870d1398dd996
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-08-08 14:50:09 +0900

    Create literal Example.java

---
+++ literal Example.java
@@ -0,0 +1,20 @@
+public class java
+{
+	public static void main(String[] args)
+	{
+		int var1 = 10;		// 10진수
+		System.out.println(var1);
+		
+		int var2 = 010;		// 8진수
+		System.out.println(var2);
+		
+		int var3 = 0x20;	// 16진수
+		System.out.println(var3);
+		
+		double var4 = 0.24;
+		System.out.println(var4);
+		
+		double var5 = 2E5;	// 2 곱하기 10의5승
+		System.out.println(var5);
+	}
+}
