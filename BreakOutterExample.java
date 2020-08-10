1a342c74aeb069d294eb9bee4c652de70b17649b
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-08-10 14:20:28 +0900

    Create BreakOutterExample.java

---
+++ BreakOutterExample.java
@@ -0,0 +1,17 @@
+public class java
+{
+	public static void main(String[] args) throws Exception
+	{
+		Outter: for(char upper = 'A'; upper <= 'Z'; upper++)
+		{
+			for(char lower = 'a'; lower <= 'z'; lower++)
+			{
+				System.out.println(upper + "_" + lower);
+				if(lower == 'g')
+				{
+					break Outter;
+				}
+			}
+		}
+	}
+}
