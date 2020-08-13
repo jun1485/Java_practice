9600d982e564d4d32e6eb9e5f982c2e2bf1be0a4
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-08-13 13:59:32 +0900

    Create Integer parseInt.java

---
+++ Integer parseInt.java
@@ -0,0 +1,16 @@
+public class java
+{
+	public static void main(String[] args)
+	{
+		String[] array = {"3", "5"};
+		
+		String strName1 = array[0];
+		String strName2 = array[1];
+		
+		int num1 = Integer.parseInt(strName1);
+		int num2 = Integer.parseInt(strName2);
+		
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+}
