fa08a79b272be2a6178bcc91565123194504de2c
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-08-19 12:49:26 +0900

    Create PrinterExample.java

---
+++ PrinterExample.java
@@ -0,0 +1,32 @@
+public class Printer
+{
+	void println(int x)
+	{
+		System.out.println(x);
+	}
+	void println(boolean x)
+	{
+		System.out.println(x);
+	}
+	void println(double x)
+	{
+		System.out.println(x);
+	}
+	void println(String x)
+	{
+		System.out.println(x);
+	}
+}
+//---------------------------------------------------
+
+public class java
+{
+	public static void main(String[] args)
+	{
+		Printer printer = new Printer();
+		printer.println(10);
+		printer.println(true);
+		printer.println(5.7);
+		printer.println("홍길동");
+	}
+}
