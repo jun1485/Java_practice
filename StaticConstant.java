3fe349c8afae160b3d49f699cd75e7843530b014
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-08-24 08:35:21 +0900

    Create StaticConstant.java

---
+++ StaticConstant.java
@@ -0,0 +1,18 @@
+public class Earth
+{
+	static final double EARTH_RADIUS = 6400;	// 상수 작성은 대문자로 하는게 관례 !
+	static final double EARTH_SURFACE_AREA;
+		
+	static
+	{
+		EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+	}
+}
+public class java
+{
+	public static void main(String[] args)
+	{
+		System.out.println("지구의 반지름 : " + Earth.EARTH_RADIUS + "km");
+		System.out.println("지구의 표면적 : " + Earth.EARTH_SURFACE_AREA + "km^2");
+	}
+}
