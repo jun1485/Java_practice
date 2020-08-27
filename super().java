446d2e0c487b238ea855c8d5802d19d609b44a60
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-08-27 20:42:59 +0900

    Create super().java

---
+++ super().java
@@ -0,0 +1,59 @@
+public class Airplane 
+{
+	public void land()
+	{
+		System.out.println("착륙합니다.");
+	}
+	public void fly()
+	{
+		System.out.println("일반비행합니다.");
+	}
+	public void takeOff()
+	{
+		System.out.println("이륙합니다.");
+	}
+}
+//------------------------------------------
+
+public class SupersonicAirplane extends Airplane
+{
+	public static final int NORMAL = 1;
+	public static final int SUPERSONIC = 2;
+	
+	public int flyMode = NORMAL;
+	
+	@Override
+	public void fly()
+	{
+		if(flyMode == SUPERSONIC)
+		{
+			System.out.println("초음속 비행합니다.");
+		}
+		else
+		{
+			super.fly();
+		}
+	}
+}
+//------------------------------------------
+
+public class SupersonicAirplaneExample
+{
+	public static void main(String[] args)
+	{
+	SupersonicAirplane sa = new SupersonicAirplane();
+	sa.takeOff();
+	
+	sa.fly();
+	
+	sa.flyMode = SupersonicAirplane.SUPERSONIC;
+	
+	sa.fly();
+	
+	sa.flyMode = SupersonicAirplane.NORMAL;
+	
+	sa.fly();
+	
+	sa.land();
+	}
+}
