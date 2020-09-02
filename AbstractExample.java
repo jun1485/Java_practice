52d593ce3e651081ec5ec3f9c17ac1e8e7140234
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-09-02 10:35:03 +0900

    Update AbstractExample.java

--- AbstractExample.java
+++ AbstractExample.java
@@ -18,3 +18,27 @@ public class Dog extends Animal
 		System.out.println("멍멍");
 	}
 }
+//------------------------------------------
+
+public class AbstractExample
+{
+	public static void main(String[] args)
+	{
+		Dog dog = new Dog();
+		
+		dog.sound();
+		System.out.println("--------------------------------");
+		
+		Animal animal = null;
+		animal = new Dog();
+		
+		animal.sound();
+		System.out.println("--------------------------------");
+		
+		animalSound(new Dog());
+	}
+	public static void animalSound(Animal animal) // 어떤 자식객체가 와도 실행 가능 !
+	{
+		animal.sound();
+	}
+}
