58551003d2741e12bca67b15628717a4fbaf32a4
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-09-04 12:25:24 +0900

    Create Constant_in_Interface.java

---
+++ Constant_in_Interface.java
@@ -0,0 +1,10 @@
+public interface RemoteControl
+{
+	int MAX_VOLUME = 10; 	// public static final이 생략됨 !
+	int MIN_VOLUME = 0;
+	
+//	static
+//	{
+//		MIN_VOLUME = 0;		// 클래스에서처럼 정적 블록 사용 불가 !
+//	}
+}
