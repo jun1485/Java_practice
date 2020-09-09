68428c947a848c3c5c4f5726b5fce928f0b23d88
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-09-09 10:05:38 +0900

    Update InterfaceExample.java

--- InterfaceExample.java
+++ InterfaceExample.java
@@ -65,5 +65,22 @@ public class SmartTelevision implements RemoteControl, Searchable
 	{
 		System.out.println("TV를 끕니다!");
 	}
-	
+
+	@Override
+	public void setVolume(int volume) 
+	{
+		if(volume > RemoteControl.MAX_VOLUME)
+		{
+			this.volume = RemoteControl.MAX_VOLUME;
+		}
+		else if(volume < RemoteControl.MIN_VOLUME)
+		{
+			this.volume = RemoteControl.MIN_VOLUME;
+		}
+		else
+		{
+			this.volume = volume;
+		}
+		System.out.println("현재 TV 볼륨 : " + volume);
+	}
 }
