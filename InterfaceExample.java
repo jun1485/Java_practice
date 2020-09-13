e3937093b686ae8265804f2f88df9c7439336675
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-09-13 13:48:02 +0900

    Update InterfaceExample.java

--- InterfaceExample.java
+++ InterfaceExample.java
@@ -55,3 +55,39 @@ public class SmartTelevision implements RemoteControl
 		System.out.println("현재 TV 볼륨 : " + volume);
 	}
 }
+//----------------------------------------
+
+public class Audio implements RemoteControl
+{
+	private int volume;
+	
+	@Override
+	public void turnOn() 
+	{
+		System.out.println("Audio를 켭니다!");
+	}
+
+	@Override
+	public void turnOff()
+	{
+		System.out.println("Audio를 끕니다!");
+	}
+
+	@Override
+	public void setVolume(int volume) 
+	{
+		if(volume > RemoteControl.MAX_VOLUME)
+		{
+			this.volume = RemoteControl.MAX_VOLUME;
+		}
+		else if(volume < RemoteControl.MIN_VOLUME)
+		{
+			this.volume = RemoteControl.MIN_VOLUME;
+		}
+		else
+		{
+			this.volume = volume;
+		}
+		System.out.println("현재 Audio 볼륨 : " + volume);
+	}
+}
