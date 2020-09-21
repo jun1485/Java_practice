3062a3142ecc3fdfbe01a26b84c20067ced49be5
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2020-09-21 16:09:47 +0900

    Update field_polymorphism.java

--- field_polymorphism.java
+++ field_polymorphism.java
@@ -17,3 +17,11 @@ public class KumhoTire implements Tire
 		System.out.println("금호 타이어가 굴러갑니다.");
 	}
 }
+
+//---------------------------------------------------
+
+public interface Tire 
+{
+	public void roll();
+}
+//---------------------------------------------------
