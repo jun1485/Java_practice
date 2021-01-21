0afc7fd849854f0bcca9c2856b5e3ff13046702b
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-01-21 21:51:52 +0900

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class scan {
+
+	public static void main(String[] args)
+	{
+		int number = 123;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("숫자 입력 : ");
