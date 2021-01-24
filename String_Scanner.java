143e5d069735f88415bcdb52872b550a1850b42a
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-01-24 17:00:25 +0900

    Create String_Scanner.java

---
+++ String_Scanner.java
@@ -0,0 +1,12 @@
+String_Scanner.java
+import java.util.Scanner;
+
+public class scan {
+
+	public static void main(String[] args)
+	{
+		String str = "hi";
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("문자열 입력 : ");
