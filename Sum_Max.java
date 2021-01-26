7d7e732de18a2ad86eb82a8e6121ce71600ee7f9
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-01-26 21:23:27 +0900

    Create Sum_Max.java

---
+++ Sum_Max.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class scan {
+
+	public static void main(String[] args)
+	{
+		System.out.print("입력할 정수의 개수 입력 : ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
