1c59b973a47fb401601f15eab875a0826f65d170
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-02-04 22:09:22 +0900

    Create IntPair.java

---
+++ IntPair.java
@@ -0,0 +1,10 @@
+IntPair.java
+import java.util.Scanner;
+
+public class IntPair
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("입력받을 정수의 개수 입력 : ");
