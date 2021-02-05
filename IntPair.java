0f8890a8687bed2fda8f9d95fe9885b135a4ef45
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-02-05 20:05:31 +0900

    Update IntPair.java

--- IntPair.java
+++ IntPair.java
@@ -1,4 +1,3 @@
-IntPair.java
 import java.util.Scanner;
 
 public class IntPair
@@ -8,3 +7,8 @@ public class IntPair
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("입력받을 정수의 개수 입력 : ");
+		int n = sc.nextInt();
+		int[] data = new int[n];
+		int count = 0;
+		
+		for(int i=0; i<n; i++)
