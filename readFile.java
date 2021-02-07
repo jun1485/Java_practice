2c0cfddfc40a12e1fe37f32bad3aa17051d472cd
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-02-07 22:30:57 +0900

    Update readFile.java

--- readFile.java
+++ readFile.java
@@ -1,5 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class readFile
 {
 	static String[] name = new String[1000];
 	static int[] number = new int[1000];
 	static int n = 0;
+	
+	public static void main(String[] args)
+	{
+		try 
+		{
