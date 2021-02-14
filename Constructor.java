de8402661b0c5976e4953b58abe0b11488f69cbe
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-02-14 22:27:44 +0900

    Update Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -18,3 +18,9 @@ public class MyRectangle
 	public int width;
 	public int height;
 	
+	public MyRectangle(int x, int y, int w, int h)
+	{
+		lu = new Mypoint(x, y);
+		width = w;
+		height = h;
+	}
