27d62ed7331dc7cc11533c2a96fd4a2994894c14
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-02-27 22:27:43 +0900

    Update OrthoLine.java

--- OrthoLine.java
+++ OrthoLine.java
@@ -23,3 +23,8 @@ public class OrthoLine
 	
 	public OrthoLine(MyPoint p, MyPoint q) 
 	{
+		u = p;
+		v = q;
+		if(p.x > q.x || p.x == q.x && p.y > q.y)
+			swap();		// 수직선의 경우 u가 y좌표가 더 작은 쪽, 수평선의 경우 u가 x좌표가 더 작은 점이 될 수 있도록
+	}
