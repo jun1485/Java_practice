98ced954c8b01c443876625682a81ab97c480138
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-04-05 22:32:11 +0900

    Create QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,8 @@
+package Algorithm;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		final int max = 10;
+		int[] A  = new int[]{40, 30, 100, 60, 80, 70, 90, 10, 20, 50};
+		int i;
