ee6cd30f919c9edc53ddf9ada3730be6a5f64460
Joon1485 <65483521+Joon1485@users.noreply.github.com>
2021-04-16 22:21:50 +0900

    Update RadixSort.java

--- RadixSort.java
+++ RadixSort.java
@@ -4,3 +4,7 @@ public class RadixSort {
 	public static void main(String[] args) {
 		int[] data = {8,26,23,11,27,501,2,34,56,256};
 		int max = 0;
+		for(int i=1; i<data.length; i = i*10) {
+			max = Math.max(data[i-1], data[i]);
+			radixSort(data, data.length, max);
+		}
